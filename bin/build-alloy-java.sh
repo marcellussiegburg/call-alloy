26388971d4d47be0e011811a7462bb82d0b66845
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2019-10-24 17:01:10 +0200

    Refine class file version

--- bin/build-alloy-java.sh
+++ bin/build-alloy-java.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 javac -cp org.alloytools.alloy.dist.jar RunAlloy.java -d .
-printf '\x30' | dd of=alloy/RunAlloy.class bs=1 seek=7 count=3 conv=notrunc
+printf '\x33' | dd of=alloy/RunAlloy.class bs=1 seek=7 count=3 conv=notrunc
